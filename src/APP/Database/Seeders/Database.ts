2ee9a5280803f02228f761dcfcf14385430c58c4
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-22 15:51:24 -0300

    updates

--- src/APP/Database/Seeders/Database.ts
+++ src/APP/Database/Seeders/Database.ts
@@ -1,6 +1,11 @@
 import { Questions } from "./Questions.js";
+import { User } from "./User.js";
 
 async function run_seeds() {
-  Questions();
+  // Questions();
+  // User();
 }
+const start = performance.now();
 run_seeds();
+const end = performance.now();
+console.log(`Execution time: ${end - start} ms`);
