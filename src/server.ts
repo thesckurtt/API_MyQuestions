fb76c9a088ed9ac589601b5d6103380227aa8e56
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-22 08:51:20 -0300

    update server

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,26 @@
 import { fastify } from "fastify";
-import dotenv from 'dotenv';
-import {APLICATION_LISTENING_PORT, APP_DEBUG} from "./APP/Config/config.js";
+import dotenv from "dotenv";
+import { APLICATION_LISTENING_PORT, APP_DEBUG } from "./APP/Config/config.js";
 import publicRoutes from "./APP/Routes/public.js";
 
-dotenv.config()
-
+dotenv.config();
 
 export const server = fastify({
-  logger: APP_DEBUG
+  logger: APP_DEBUG,
 });
 
-server.register(publicRoutes, {prefix: "/api/v1"})
+server.register(publicRoutes, { prefix: "/api/v1" });
 
-// server.get("/", (request, reply)=>{
-//   return "hello";
-// });
-
-
-console.log();
-
-server.listen({
-  host: "0.0.0.0",
-  port: APLICATION_LISTENING_PORT
-}, (error, address)=>{
-  if(error){
-    console.log(error) 
-  }else{
-    console.log("\n Application in URL: " + address + "\n");
+server.listen(
+  {
+    host: "0.0.0.0",
+    port: APLICATION_LISTENING_PORT,
+  },
+  (error, address) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("\n Application in URL: " + address + "\n");
+    }
   }
-});
+);
