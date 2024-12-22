0f3166de185fa167a204f39aeae3450939c4194d
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-21 21:31:39 -0300

    updates

--- src/APP/Controllers/PublicController.ts
+++ src/APP/Controllers/PublicController.ts
@@ -1,5 +1,18 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 
 export async function question(request: FastifyRequest, reply: FastifyReply) {
-  return "controller question"
+  const json = {
+    id: "3e1c7b69-a5c3-46d9-9f1b-9a8d58d4db23",
+    title: "What is the capital of France?",
+    description: "Choose the correct alternative for the capital city of France.",
+    alternatives: {
+      a: "Berlin",
+      b: "Madrid",
+      c: "Paris",
+      d: "Rome"
+    },
+    correct_alternative: "c"
+  }
+  
+  return json;
 }
