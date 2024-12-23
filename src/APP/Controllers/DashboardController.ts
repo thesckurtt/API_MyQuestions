fed1cc89f5162a378947a4c29bf292722d5a0361
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2024-12-22 22:18:17 -0300

    update dashboard routes

--- src/APP/Controllers/DashboardController.ts
+++ src/APP/Controllers/DashboardController.ts
@@ -1,12 +1,44 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { prisma } from "../Config/db_config.js";
+import { Console, error } from "console";
 
-interface QuestionView {
+interface QuestionInterface {
   id: string;
+  title: string;
+  description?: string;
+  alternativeA: string;
+  alternativeB: string;
+  alternativeC: string;
+  alternativeD: string;
+  correctAlternative: string;
 }
 
+// interface ViewQuestionParamsInterface {
+//   id: string;
+// }
+// interface AddQuestionBodyInterface {
+//   title: string;
+//   description?: string;
+//   alternativeA: string;
+//   alternativeB: string;
+//   alternativeC: string;
+//   alternativeD: string;
+//   correctAlternative: string;
+// }
+// interface editQuestionInterface {
+//   id: string;
+// }
+
+export async function getAllQuestion(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const questions = await prisma.questions.findMany();
+
+  return questions;
+}
 export async function viewQuestion(
-  request: FastifyRequest<{Params: QuestionView }>,
+  request: FastifyRequest<{ Params: QuestionInterface }>,
   reply: FastifyReply
 ) {
   const questionId = request.params.id ?? "";
@@ -17,29 +49,125 @@ export async function viewQuestion(
     },
   });
 
-  if(question === null){
+  if (question === null) {
     return reply.code(404).send();
   }
   return question;
 }
 
 export async function addQuestion(
-  request: FastifyRequest,
+  request: FastifyRequest<{ Body: QuestionInterface }>,
   reply: FastifyReply
 ) {
-  return "addQuestion";
+  if (!request.body) {
+    return reply.code(400).send({ error: "Request body is required" });
+  }
+  const {
+    title,
+    description,
+    alternativeA,
+    alternativeB,
+    alternativeC,
+    alternativeD,
+    correctAlternative,
+  } = request.body || {};
+  console.log(
+    title,
+    description,
+    alternativeA,
+    alternativeB,
+    alternativeC,
+    alternativeD,
+    correctAlternative
+  );
+  const dataQuestion = {
+    title,
+    description,
+    alternativeA,
+    alternativeB,
+    alternativeC,
+    alternativeD,
+    correctAlternative,
+  };
+  const result = await prisma.questions.create({ data: dataQuestion });
+  console.log(result);
+
+  return result;
 }
 
 export async function editQuestion(
-  request: FastifyRequest,
+  request: FastifyRequest<{
+    Params: QuestionInterface;
+    Body: QuestionInterface;
+  }>,
   reply: FastifyReply
 ) {
-  return "editQuestion";
+  if (!request.params.id) {
+    return reply.code(400).send({ error: "missing id parameter" });
+  }
+
+  if (!request.body) {
+    return reply.code(400).send({ error: "Request body is required" });
+  }
+  const {
+    title,
+    description,
+    alternativeA,
+    alternativeB,
+    alternativeC,
+    alternativeD,
+    correctAlternative,
+  } = request.body || {};
+
+  const questionId = request.params.id ?? null;
+
+  const question = await prisma.questions.findFirst({
+    where: {
+      id: questionId,
+    },
+  });
+
+  const result = await prisma.questions.update({
+    where: { id: questionId },
+    data: {
+      title,
+      description,
+      alternativeA,
+      alternativeB,
+      alternativeC,
+      alternativeD,
+      correctAlternative,
+    },
+  });
+
+  return result;
 }
 
 export async function deleteQuestion(
-  request: FastifyRequest,
+  request: FastifyRequest<{ Params: QuestionInterface }>,
   reply: FastifyReply
 ) {
-  return "deleteQuestion";
+  if (!request.params.id) {
+    return reply.code(400).send({ error: "missing id parameter" });
+  }
+
+  const questionId = request.params.id;
+
+  const question = await prisma.questions.findFirst({
+    where: {
+      id: questionId,
+    },
+  });
+
+  if (!question) {
+    return reply.code(404).send();
+  }
+
+  const result = await prisma.questions.delete({
+    where: {
+      id: questionId,
+    },
+  });
+
+  return result;
 }
--- src/APP/Routes/dashboard.ts
+++ src/APP/Routes/dashboard.ts
@@ -1,16 +1,25 @@
 import { FastifyInstance } from "fastify";
-import { addQuestion, editQuestion, deleteQuestion, viewQuestion } from "../Controllers/DashboardController.js"
+import {
+  addQuestion,
+  editQuestion,
+  deleteQuestion,
+  viewQuestion,
+  getAllQuestion
+} from "../Controllers/DashboardController.js";
 export async function dashboardRoutes(fastify: FastifyInstance) {
   fastify.addHook("onRequest", fastify.authenticate);
 
   fastify.get("/", () => {
     return "Welcome";
   });
-  fastify.get("/question/view/:id", viewQuestion)
 
-  fastify.post("/question/add", addQuestion)
+  fastify.get("/questions/all", getAllQuestion)
 
-  fastify.put("/question/edit/:id", editQuestion)
+  fastify.get("/question/view/:id", viewQuestion);
 
-  fastify.delete("/question/delete/:id", deleteQuestion)
+  fastify.post("/question/add", addQuestion);
+
+  fastify.put("/question/edit/:id", editQuestion);
+
+  fastify.delete("/question/delete/:id", deleteQuestion);
 }
